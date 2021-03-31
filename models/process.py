f3476b915c0c9fffeed70d61be3bb54036317f27
ganli <ganli@yo-star.com>
2021-03-31 23:10:05 +0800

    added evaluate function

--- models/process.py
+++ models/process.py
@@ -4,7 +4,11 @@ from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 from pm4py.algo.simulation.playout import simulator
 from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
 from pm4py.visualization.petrinet import visualizer as pn_visualizer
+from pm4py.visualization.graphs import visualizer as graphs_visualizer
+from pm4py.util import constants
+from pm4py.statistics.traces.log import case_statistics
 
+from pm4py.algo.evaluation.replay_fitness import evaluator as replay_fitness_evaluator
 
 class Process:
     def __init__(self, event_data):
@@ -14,8 +18,9 @@ class Process:
     def train(self):
         #Alpha Miner
 
-        #self.net, self.initial_marking, self.final_marking = alpha_miner.apply(self.event_data)
-        self.net, self.initial_marking, self.final_marking = heuristics_miner.apply(self.event_data, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
+        self.net, self.initial_marking, self.final_marking = alpha_miner.apply(self.event_data)
+        #Heuristics Miner
+        #self.net, self.initial_marking, self.final_marking = heuristics_miner.apply(self.event_data, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
 
 
     def generate(self, n=100):
@@ -23,19 +28,29 @@ class Process:
         simulated_log = simulator.apply(self.net, self.initial_marking,self.final_marking, variant=simulator.Variants.BASIC_PLAYOUT, parameters={simulator.Variants.BASIC_PLAYOUT.value.Parameters.NO_TRACES: n})
         return simulated_log
 
-    def visual(self):
+    def evaluate(self,log):
         
-        gviz = pn_visualizer.apply(self.net, self.initial_marking, self.final_marking)
-        pn_visualizer.view(gviz)
+        #gviz = pn_visualizer.apply(self.net, self.initial_marking, self.final_marking)
+        #pn_visualizer.view(gviz)
+        #x, y = case_statistics.get_kde_caseduration(log, parameters={constants.PARAMETER_CONSTANT_TIMESTAMP_KEY: "time:timestamp"})
+        #gviz = graphs_visualizer.apply_plot(x, y, variant=graphs_visualizer.Variants.CASES)
+        #graphs_visualizer.view(gviz)
+
+        #gviz = graphs_visualizer.apply_semilogx(x, y, variant=graphs_visualizer.Variants.CASES)
+        #graphs_visualizer.view(gviz)
+        
+        fitness = replay_fitness_evaluator.apply(log, self.net, self.initial_marking, self.final_marking, variant=replay_fitness_evaluator.Variants.TOKEN_BASED)
+        print(fitness)                            
 
 if __name__ == "__main__":
-    #event_data =xes_importer.apply(os.path.join("..", "tests", "input_data", "running-example.xes"))
-    event_data =xes_importer.apply(os.path.join("..", "tests", "compressed_input_data", "09_a32f0n00.xes.gz"))
+    #event_data =xes_importer.apply(os.path.join("tests", "input_data", "running-example.xes"))
+    #print(os.path.abspath(os.path.join( "tests", "input_data", "running-example.xes")))
+    event_data =xes_importer.apply(os.path.join("tests", "compressed_input_data", "09_a32f0n00.xes.gz"))
     pm = Process(event_data)
     pm.train()
-    p10=pm.generate(10)
+    p10=pm.generate(1000)
     p20=pm.generate(20)
     print(p10)
     print(p20)
-    pm.visual()
+    pm.evaluate(p10)
 
